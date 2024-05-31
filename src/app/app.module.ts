ed7826a5f4f034c9f645625d1dad35b507bb64cb
Tx <tahersouissi40@gmail.com>
2024-05-31 14:49:59 +0100

    adding nav bar and comonenets and db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
